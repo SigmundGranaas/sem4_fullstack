4ca8cfbcd47e8a4a898d8542c81877740c0f2576
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-02-16 17:38:45 +0100

    * first working database and spring combination

---
+++ src/test/java/no/gretkas/ov2/Ov2ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.gretkas.ov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ov2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
